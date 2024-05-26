af0ac0d64f005feda83b48b5bca88410847fdf89
achiorean <adrianioan.chiorean@gmail.com>
2024-05-26 11:16:55 +0300

    update pe Issue

---
+++ ScooterService/Migrations/IssueSeed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScooterService.Migrations
+{
+    public partial class IssueSeed : Migration
+     {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Issues",
+                columns: new[] { "Id", "Name", "HoursOfWork", "ReparationId" },
+                values: new object[] { 1L, "Schimbare roata", 2, 1L});
+
+
+        }
+    }
+}
