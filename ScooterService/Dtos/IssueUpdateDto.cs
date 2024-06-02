d9421eafed2df2cb734547cffa1cfb13e6c6d533
dunmer0 <popa.catalin88@gmail.com>
2024-06-02 12:19:54 +0300

    Fixed issues.

--- ScooterService/Dtos/IssueUpdateDto.cs
+++ ScooterService/Dtos/IssueUpdateDto.cs
@@ -3,14 +3,9 @@
     public class IssueUpdateDto
     {
         public int Id { get; set; }
-<<<<<<< HEAD
         public string name { get; set; }
-
         public int HoursOfWork { get; set; }  
-        
+     
 
-=======
-        public int HoursOfWorks { get; set; }
->>>>>>> da85f405fd7da9bd577d1fae7242c300ba6f7b52
     }
 }
